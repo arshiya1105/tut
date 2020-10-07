6dc196c0e073c0cb0f6c3a5f6a1245dfdd37dfe1
arshiya <arshiya.ux@gmail.com>
2020-10-07 16:03:25 +0530

    second

--- app.js
+++ app.js
@@ -19,23 +19,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req,res)=>{
+  res.render('index');
+})
+app.get('/make', (req,res)=>{
+  //res.send('Hello Arshiya!'); 
+  res.status(404).end();
+ 
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
